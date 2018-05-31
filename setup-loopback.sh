d8203c46b32ba01be4082379aacdfe8f0ea77770
Luke Bakken <luke@bakken.io>
2018-05-31 11:39:30 -0700

    tweak

--- setup-loopback.sh
+++ setup-loopback.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
+set -e
+
 ip addr add 127.0.0.2/8 dev lo                                                                                                                                                                                                                       
 ip addr add 127.0.0.3/8 dev lo
 ip addr add 127.0.0.4/8 dev lo                                                                                                                                                                                                                       
