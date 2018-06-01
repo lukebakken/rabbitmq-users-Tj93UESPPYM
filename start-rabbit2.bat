157eb71483927841595cd3aa3687fee6d9f2e4f3
Luke Bakken <luke@bakken.io>
2018-06-01 11:06:18 -0700

    fixup

--- start-rabbit2.bat
+++ start-rabbit2.bat
@@ -4,4 +4,4 @@ set RABBITMQ_NODE_IP_ADDRESS=10.0.50.61
 set RABBITMQ_DIST_PORT=25672
 set RABBITMQ_SERVER_START_ARGS=-rabbitmq_management listener [{ip,"10.0.50.61"},{port,15672}]
 set RABBITMQ_SERVER_ADDITIONAL_ERL_ARGS=-kernel inet_dist_use_interface {10,0,50,61}
-"C:\Program Files\RabbitMQ\rabbitmq_server-3.7.5\sbin\rabbitmq-server.bat"
+"C:\Program Files\RabbitMQ Server\rabbitmq_server-3.7.5\sbin\rabbitmq-server.bat"
