85007b115beb2d6a612f0cd2f7149390b5ad88e4
Luke Bakken <luke@bakken.io>
2018-06-01 11:50:00 -0700

    fixup

--- start-rabbit3.bat
+++ start-rabbit3.bat
@@ -2,6 +2,6 @@ set RABBITMQ_ALLOW_INPUT=true
 set RABBITMQ_NODENAME=rabbit@WIN2012R2-3
 set RABBITMQ_NODE_IP_ADDRESS=10.0.50.62
 set RABBITMQ_DIST_PORT=25672
-set RABBITMQ_SERVER_START_ARGS=-rabbitmq_management listener [{ip,"10.0.50.62"},{port,15672}]
+REM set RABBITMQ_SERVER_START_ARGS=-rabbitmq_management listener [{ip,"10.0.50.62"},{port,15672}]
 set RABBITMQ_SERVER_ADDITIONAL_ERL_ARGS=-kernel inet_dist_use_interface {10,0,50,62}
 "C:\Program Files\RabbitMQ Server\rabbitmq_server-3.7.5\sbin\rabbitmq-server.bat"
