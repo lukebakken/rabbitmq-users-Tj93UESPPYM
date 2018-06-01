1a45fe32b2be0aed7ecc78a90a8f98fef6763cd7
Luke Bakken <luke@bakken.io>
2018-06-01 11:28:52 -0700

    fixup

--- create-cluster.bat
+++ create-cluster.bat
@@ -1,9 +1,9 @@
 call .\rabbitmqctl.bat -n rabbit@WIN2012R2-2 stop_app
-call .\rabbitmqctl.bat -n rabbit@WIN2012R2-2 join_cluster rabbit@WIN2012R2-R1
+call .\rabbitmqctl.bat -n rabbit@WIN2012R2-2 join_cluster rabbit@WIN2012R2-1
 call .\rabbitmqctl.bat -n rabbit@WIN2012R2-2 start_app
 
 call .\rabbitmqctl.bat -n rabbit@WIN2012R2-3 stop_app
-call .\rabbitmqctl.bat -n rabbit@WIN2012R2-3 join_cluster rabbit@WIN2012R2-R1
+call .\rabbitmqctl.bat -n rabbit@WIN2012R2-3 join_cluster rabbit@WIN2012R2-1
 call .\rabbitmqctl.bat -n rabbit@WIN2012R2-3 start_app
 
 call .\rabbitmqctl.bat -n rabbit@WIN2012R2-1 cluster_status
