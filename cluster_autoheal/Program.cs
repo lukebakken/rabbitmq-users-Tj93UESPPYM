efceb2c5e2d1229d11080b20a765f35a2f8e071b
Luke Bakken <luke@bakken.io>
2018-06-05 12:36:49 -0700

    only use one connection

--- cluster_autoheal/Program.cs
+++ cluster_autoheal/Program.cs
@@ -12,25 +12,27 @@ namespace cluster_autoheal
             cf.AutomaticRecoveryEnabled = true;
             cf.TopologyRecoveryEnabled = true;
             cf.AuthMechanisms = new AuthMechanismFactory[] { new PlainMechanismFactory() };
-            cf.UserName = "guest";
-            cf.Password = "guest";
+            cf.UserName = "lbakken";
+            cf.Password = "test1234";
             cf.VirtualHost = "/";
             var h1 = new AmqpTcpEndpoint("localhost", 5060);
             var h2 = new AmqpTcpEndpoint("localhost", 5061);
             var h3 = new AmqpTcpEndpoint("localhost", 5062);
-            for (var i = 1; i <= 10; i++)
+            using (var cn = cf.CreateConnection(new[] { h1, h2, h3 }))
             {
-                using (var cn = cf.CreateConnection(new[] { h1, h2, h3 }))
-                using (var model = cn.CreateModel())
+                for (var i = 1; i <= 10; i++)
                 {
-                    var z = model.QueueDeclare($"ha.queue_autoDeleted_{i}", false, false, true);
-                    model.BasicPublish("", z.QueueName, false, model.CreateBasicProperties(), new byte[] { 1, 2, 3 });
-                    z = model.QueueDeclare($"ha.queue_durable_{i}", true, false, false);
-                    model.BasicPublish("", z.QueueName, false, model.CreateBasicProperties(), new byte[] { 1, 2, 3 });
-                    z = model.QueueDeclare($"ha.queue{i}", false, false, false);
-                    model.BasicPublish("", z.QueueName, false, model.CreateBasicProperties(), new byte[] { 1, 2, 3 });
+                    using (var model = cn.CreateModel())
+                    {
+                        var z = model.QueueDeclare($"ha.queue_autoDeleted_{i}", false, false, true);
+                        model.BasicPublish("", z.QueueName, false, model.CreateBasicProperties(), new byte[] { 1, 2, 3 });
+                        z = model.QueueDeclare($"ha.queue_durable_{i}", true, false, false);
+                        model.BasicPublish("", z.QueueName, false, model.CreateBasicProperties(), new byte[] { 1, 2, 3 });
+                        z = model.QueueDeclare($"ha.queue{i}", false, false, false);
+                        model.BasicPublish("", z.QueueName, false, model.CreateBasicProperties(), new byte[] { 1, 2, 3 });
+                    }
+                    Console.WriteLine(i);
                 }
-                Console.WriteLine(i);
             }
             Console.WriteLine("Press any key");
             Console.ReadKey();
